37dbaac053dbd97b797eb6e4fcbb7a2e4fa2eae1
Stephen-we <97359718+Stephen-we@users.noreply.github.com>
2024-08-27 21:03:26 +0530

    Add files via upload

---
+++ Constructor .java
@@ -0,0 +1,23 @@
+     // Java Constructor
+import java.util.*;
+
+class A
+{
+  int a; String name;
+//  A()
+//  {
+//   a = 0; name=null;
+//  }
+ void show()
+ {
+  System.out.print(a+ " "+name);
+ }
+}
+class B
+{
+  public static void main(String[] args) {
+    
+    A ref=new A();
+    ref.show();
+  }
+}
